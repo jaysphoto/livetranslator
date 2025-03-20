f5c3797551226b8aff23fb32c9694f470a43dd4f
Ian Moss <alterisian@users.noreply.github.com>
2025-03-20 18:18:20 +0000

    WIP.  DisplayTranslation display_live_text and rspec.
    Co-authored-by: Greg Blumenthal <greg@gmbs-consulting.com>
    Co-authored-by: Jay aka. Geoffrey de Kleijn <jaysphoto@users.noreply.github.com>
    Co-authored-by: petribox <petribox@users.noreply.github.com>
    Co-authored-by: alterisian <alterisian@users.noreply.github.com>

---
+++ display_translation.rb
@@ -0,0 +1,25 @@
+require 'logger'
+
+class DisplayTranslation
+    def initialize
+        @logger = Logger.new(STDOUT)
+        @logger.level = Logger::DEBUG
+        @live_text_dir = 'live_text'
+    end
+
+    def display_live_text
+        @logger.info("Scanning #{@live_text_dir} for text files...")
+        last_text_file = Dir.glob(File.join(@live_text_dir, '*')).last
+        if last_text_file
+          file_text = File.open(last_text_file).read
+          @logger.info("last file: #{file_text}")
+        end
+    end
+    
+end
+
+if __FILE__ == $PROGRAM_NAME
+    # Display Translation
+    display = DisplayTranslation.new
+    display.display_live_text
+  end

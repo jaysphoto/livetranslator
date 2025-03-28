762ec67a6264da3b26694ffe5ea946600fc5034d
Ian moss <ian@alter.is>
2025-03-28 13:14:45 +0100

    updates to Gemfile and app

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '3.2.2' # 3.3.7
+ruby '3.2.2' # Other options: 3.2.8 (Heroku, 3.2.x latest), 3.3.7
 
 # gem 'puma'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@
 require 'sinatra'
 
 get '/' do
-  'Hello, World!'
+  # Changes to the text here require a restart of the Sinatra server (puma)
+  'Hello, World! Boquercom speaking...'
 end
+
+# Question: What end points are needed?
+# Ideas:
+# /start with stream_url - this could be the root
+# /stop with stream_id? or just stream_url
+# /display/stream_id - is it possible without a db? Can we avoid a db initially?
+# /transcrition/stream_id_or_url - gives all the text for the recent stream transcription/translation
+
+# Question: What is the most basic needed vs actually nice. i.e. we don't NEED a great font, but will make it more readable.
+
+# Question: Do we just display the translation? Or original transcription too? Is it a complication to do both i.e. reduce usability? How do we test that? 2 events? 1 with/1 without.

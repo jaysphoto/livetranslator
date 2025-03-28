420f0f85a8cc7c7a9aec69231fa732beaa07aab7
Ian moss <ian@alter.is>
2025-03-28 13:40:34 +0100

    Making the output from one our initial volunteered whatsapp voice messages. Looks good at 400% on chrome

--- app.rb
+++ app.rb
@@ -3,16 +3,18 @@ require 'sinatra'
 
 get '/' do
   # Changes to the text here require a restart of the Sinatra server (puma)
-  'Hello, World! Boquercom speaking...'
+  'Hello, World! Boquercom speaking...<br/>
+  <br/>To work in product, it is necessary to have a multidisciplinary team <br/> with various profiles, which ensures that new functionalities <br/> are delivered with each development.
+  '
 end
 
 # Question: What end points are needed?
 # Ideas:
-# /start with stream_url - this could be the root
-# /stop with stream_id? or just stream_url
-# /display/stream_id - is it possible without a db? Can we avoid a db initially?
+# /start with stream_url - this could be the root. Any additional options? i.e. 4 seconds/EN and/or ES output?
+# /stop with stream_id_or_url or just stream_url
+# /display/stream_id_or_url - is it possible without a db? Can we avoid a db initially?
 # /transcrition/stream_id_or_url - gives all the text for the recent stream transcription/translation
 
 # Question: What is the most basic needed vs actually nice. i.e. we don't NEED a great font, but will make it more readable.
-
 # Question: Do we just display the translation? Or original transcription too? Is it a complication to do both i.e. reduce usability? How do we test that? 2 events? 1 with/1 without.
+
